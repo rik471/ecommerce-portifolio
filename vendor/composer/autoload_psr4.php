d8187c4a1773bf7d1b00c2b73aaeb118f199016d
rik471 <riksongleison471@gmail.com>
2018-02-17 14:39:32 -0200

    autoload de classes

--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -6,5 +6,5 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
-    '' => array($vendorDir . '/rik471/php-classes/src'),
+    'Hcode\\' => array($vendorDir . '/rik471/php-classes/src'),
 );
